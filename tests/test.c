dddb827ee304102db50338df8e6f8cabe6748025
Daniel Hong <daniel@int16.org>
2014-02-20 21:24:08 -0500

    More test cases

--- tests/test.c
+++ tests/test.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "../include/hashtable.h"
 
 int main()
@@ -14,6 +13,17 @@ int main()
     printf("key2 = %s\n", ht->get(ht, "key2", "not found"));
     printf("key3 = %s\n", ht->get(ht, "key3", "not found"));
     printf("key4 = %s\n", ht->get(ht, "key4", "not found"));
+    
+    printf("modifying key1...\n");
+    ht->update(ht, "key1", "key1 value was modified");
+    printf("key1 = %s\n", ht->get(ht, "key1", "not found"));
+    
+    if (ht->contains_key(ht, "key3")) {
+        printf("key3 exists!\n");
+    }
+    else {
+        printf("key3 doesn't exists!\n");
+    }
     
     free_hashtable(ht);
     
